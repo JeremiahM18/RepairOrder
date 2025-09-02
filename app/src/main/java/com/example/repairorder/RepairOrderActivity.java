4cc8a16639ab119a7075fec3441f3bdca2cd2786
Jeremiah <jermcdon18@gmail.com>
2025-09-02 12:19:22 -0500

    Wrote code to set and get values.

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.repairorder;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class RepairOrderActivity extends AppCompatActivity {
+
+    double numbers = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_repair_order);
@@ -20,5 +27,19 @@ public class RepairOrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        TextView totalTextView = findViewById(R.id.total);
+
+        String value = totalTextView.getText().toString();
+
+        Random gen = new Random();
+
+        double random = gen.nextDouble();
+        String v1 = "$ " + random;
+        totalTextView.setText(v1);
+
+        Log.i("TEST", value);
     }
 }
